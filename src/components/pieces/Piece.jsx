1dd37a462b409f6985d00682e5df9eabc1e98463
SimonCROS <scros@edenschool.fr>
2019-10-16 08:59:05 +0200

    Bug Tour résolue

--- src/components/pieces/Piece.jsx
+++ src/components/pieces/Piece.jsx
@@ -2,20 +2,20 @@ import PieceRender from './PieceRender';
 
 export default class Piece extends PieceRender {
 
-  constructor(props, type) {
-    super(props);
-    this.type = type;
-    this.state={location: props.defaultLocation};
-    this.id = props.registerId;
-    props.game.register(this, this.id);
-    this.movements = 0;
-  }
+    constructor(props, type) {
+        super(props);
+        this.type = type;
+        this.state={location: props.defaultLocation};
+        this.id = props.registerId;
+        props.game.register(this, this.id);
+        this.movements = 0;
+    }
 
-  move(location) {
-    this.movements++;
-    this.setState({location: location});
-    return true;
-  }
+    move(location) {
+        this.movements++;
+        this.setState({location: location});
+        return true;
+    }
 
     canAttack(search, changements, tested) {
         let canAttack = false;
@@ -53,7 +53,6 @@ export default class Piece extends PieceRender {
         let attackPossibilities = [];
         this.possibilities.forEach(e => {
             let start = e.start || 1;
-            console.log(1);
             for (let i = start; i < (e.width + start); i++) {
                 let location = {x: this.state.location.x + (e.x * i), y: this.state.location.y + (e.y * i)};
                 if (location.x < 0 || location.x > 7 || location.y < 0 || location.y > 7) continue;
@@ -74,14 +73,14 @@ export default class Piece extends PieceRender {
           });
         }
         return {glows: movePossibilities, attacked: attackPossibilities, roques: [], from: this.state.location};
-  }
-
-  onClick = () => {
-    if (this.props.game.state.team !== this.props.color) return;
-    if (this.props.game.getSelected() === this) {
-      this.props.game.resetClicked();
-      return;
     }
-    this.props.game.setSelected(this);
-  }
+
+    onClick = () => {
+        if (this.props.game.state.team !== this.props.color) return;
+        if (this.props.game.getSelected() === this) {
+            this.props.game.resetClicked();
+            return;
+        }
+        this.props.game.setSelected(this);
+    }
 }
