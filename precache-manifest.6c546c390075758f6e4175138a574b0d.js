2dd77abb032725776667e010963d9ee053e39fe3
SimonCROS <scros@edenschool.fr>
2019-10-25 14:45:38 +0200

    Updates

---
+++ precache-manifest.6c546c390075758f6e4175138a574b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79e23121d675180a8fd1066e142ecfa",
+    "url": "/echecs/index.html"
+  },
+  {
+    "revision": "f11cdf6cf4ca37540805",
+    "url": "/echecs/static/css/main.dc02d842.chunk.css"
+  },
+  {
+    "revision": "47e747b0dd77c598b75f",
+    "url": "/echecs/static/js/2.2087efd5.chunk.js"
+  },
+  {
+    "revision": "f11cdf6cf4ca37540805",
+    "url": "/echecs/static/js/main.508bc064.chunk.js"
+  },
+  {
+    "revision": "dcfc402109319650daff",
+    "url": "/echecs/static/js/runtime-main.b25529f7.js"
+  }
+]);
